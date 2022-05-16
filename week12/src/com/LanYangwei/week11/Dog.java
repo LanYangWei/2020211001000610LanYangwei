652335c095129c60d488a42e0da48f2d5001f5a4
XIE <2783242004@qq.com>
2022-05-16 16:28:39 +0800

    homework week12

---
+++ week12/src/com/LanYangwei/week11/Dog.java
@@ -0,0 +1,13 @@
+package com.LanYangwei.week11;
+
+public class Dog {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
